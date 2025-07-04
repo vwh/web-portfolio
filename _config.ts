61b95af08c7f58f0d86c5590f9621455fc4ef4f6
vwh <vwhe@proton.me>
2025-07-04 11:49:05 +0300

    build: enable css and js minification

--- _config.ts
+++ _config.ts
@@ -4,6 +4,8 @@ import jsx from "lume/plugins/jsx.ts";
 import tailwindcss from "lume/plugins/tailwindcss.ts";
 import minifyHTML from "lume/plugins/minify_html.ts";
 import robots from "lume/plugins/robots.ts";
+import lightningcss from "lume/plugins/lightningcss.ts";
+import terser from "lume/plugins/terser.ts";
 
 const site = lume({
   src: "./src",
@@ -12,6 +14,8 @@ const site = lume({
 site.use(jsx());
 site.use(tailwindcss());
 site.use(minifyHTML());
+site.use(lightningcss());
+site.use(terser());
 site.use(
   robots({
     rules: [
